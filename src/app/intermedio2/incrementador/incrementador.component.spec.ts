6a2223245e573fa706fadd6ee74b9b4322893f24
omar.aceves <omar.aceves@go-sharp.com>
2019-08-29 10:21:05 -0500

    iniciando pruebas avanzadas

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -32,4 +32,34 @@ describe('Incremendator Component', () => {
 
     });
 
+    it('Debe de mostrar en input valor progreso', () => {
+        component.cambiarValor(5);
+        fixture.detectChanges();
+        fixture.whenStable().then( () => {
+
+            const input: HTMLInputElement = fixture.debugElement.query( By.css('input') ).nativeElement;
+            console.log(input);
+            expect( input.value ).toBe('55');
+        });
+    });
+
+    it('Debe de incrementar/decrementar con click', () => {
+        const botones = fixture.debugElement.queryAll( By.css( '.btn-primary' ) );
+
+        botones[0].triggerEventHandler('click',null);
+        expect( component.progreso ).toBe(45);
+        botones[1].triggerEventHandler('click',null);
+        expect( component.progreso ).toBe(50);
+    });
+
+    it('En titulo debe mostrar el progreso', () => {
+        const botones = fixture.debugElement.queryAll( By.css('.btn-primary') );
+        botones[0].triggerEventHandler('click', null);
+
+        fixture.detectChanges();
+
+        const elem: HTMLElement = fixture.debugElement.query( By.css('h3') ).nativeElement;
+
+        expect( elem.innerHTML ).toContain('45');
+    });
 });
